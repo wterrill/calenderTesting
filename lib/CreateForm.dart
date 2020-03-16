bca6f7ae5831892af16ffcfe25f313e9c94b160b
William Terrill <william.terrill@gmail.com>
2020-03-15 20:30:13 -0500

    made the make appointment dialog box look less like crap

--- lib/CreateForm.dart
+++ lib/CreateForm.dart
@@ -24,45 +24,68 @@ class _CreateFormState extends State<CreateForm> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text(widget.day.toString() + " - " + widget.times.toString()),
+          Container(
+            color: Colors.greenAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  widget.day.toString() + " - " + widget.times.toString(),
+                  textAlign: TextAlign.center),
+            ),
+          ),
           Padding(
             padding: EdgeInsets.all(8.0),
             child: TextFormField(
               controller: _placeController,
               decoration: const InputDecoration(
-                icon: Icon(Icons.person),
+                icon: Icon(Icons.place),
                 hintText: 'Ex: Place #7',
                 labelText: 'Site Name',
               ),
             ),
           ),
-          DropDown(
-              onSelected: (String val) {
-                selectedDuration = val;
-                print("val: $val");
-                print("selectedColor: $selectedColor");
-              },
-              selectionList: [
-                '0.5',
-                '1.0',
-                '1.5',
-                '2.0',
-                '2.5',
-                '3.0',
-                '3.5',
-                '4.0',
-                '4.5',
-                '5.0',
-              ],
-              initialSelection: "2.0"),
-          DropDown(
-              onSelected: (String val) {
-                selectedColor = val;
-                print("val: $val");
-                print("selectedDuration: $selectedDuration");
-              },
-              selectionList: ['Purple', 'Yellow', 'Green', 'Orange'],
-              initialSelection: "Purple"),
+          Row(
+            children: <Widget>[
+              Text("Duration"),
+              Container(width: 60),
+              DropDown(
+                  onSelected: (String val) {
+                    selectedDuration = val;
+                    print("val: $val");
+                    print("selectedColor: $selectedColor");
+                  },
+                  selectionList: [
+                    '0.5',
+                    '1.0',
+                    '1.5',
+                    '2.0',
+                    '2.5',
+                    '3.0',
+                    '3.5',
+                    '4.0',
+                    '4.5',
+                    '5.0',
+                  ],
+                  initialSelection: "2.0"),
+              Container(width: 20),
+              Text("Hours")
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text("Color"),
+              Container(width: 60),
+              DropDown(
+                  onSelected: (String val) {
+                    selectedColor = val;
+                    print("val: $val");
+                    print("selectedDuration: $selectedDuration");
+                  },
+                  selectionList: ['Purple', 'Yellow', 'Green', 'Orange'],
+                  initialSelection: "Purple"),
+            ],
+          ),
+
           // Padding(
           //   padding: EdgeInsets.all(8.0),
           //   child: TextFormField(),
